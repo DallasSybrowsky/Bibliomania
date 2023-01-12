859079b129e50b5e62a2237a311949b58d115c75
Risvi-Tareq <ivsirqerat@gmail.com>
2023-01-11 19:52:35 -0800

    test

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS `techblog_db`;
+DROP DATABASE IF EXISTS `bibliomania_db`;
 
 -- CREATE DATABASE
-CREATE DATABASE `techblog_db`;
+CREATE DATABASE `bibliomania_db`;
