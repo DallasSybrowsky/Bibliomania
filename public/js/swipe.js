952b67998ce81f6e22f5d45b92243b340bd5ebb1
Risvi-Tareq <ivsirqerat@gmail.com>
2023-01-12 18:50:17 -0800

    tried to modify swipejs

---
+++ public/js/swipe.js
@@ -0,0 +1,15 @@
+var getISBN = function (ISBN) {
+  fetch(`https://covers.openlibrary.org/a/olid/240727-S.jpg`, {
+    method: "GET",
+    redirect: "follow",
+  }).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var chosenISBN =
+          data.results[Math.floor(Math.random() * data.results.length)];
+      });
+    }
+  });
+};
+
+console.log()
