d047b10ad269bb4777e0398d6eafa447f6f10ac8
ana <ana@anniecannons.com>
2023-01-17 19:11:18 -0800

    added js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,83 @@
-// async function signupFormHandler(event) {
-//   event.preventDefault();
-//   const username = document.querySelector("#username-signup").value.trim();
-//   const password = document.querySelector("#password-signup").value.trim();
-//   if (username && password) {
-//     const response = await fetch("/api/users", {
-//       method: "post",
-//       body: JSON.stringify({
-//         username,
-//         password,
-//       }),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     if (response.ok) {
-//       document.location.replace("/dashboard");
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// }
-// document
-//   .querySelector(".signup-form")
-//   .addEventListener("submit", signupFormHandler);
+// this is the buttons for the sliders 
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
+// sliders
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
+// sliders
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+// this is the js for functionality 
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+// this is for the sliders
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+// this is for the functionality 
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
+
+
+
+  
