023453d336e79c4239b5210cb8e510d4045de8ff
ana <ana@anniecannons.com>
2023-01-17 18:22:56 -0800

    added the dign up and handlebars js

---
+++ public/js/swipe.js
@@ -0,0 +1,41 @@
+/*
+ ======== Instagram - ronakgiriraj ======== 
+ ======== Github - ronakgiriraj ======== 
+ ======== Youtube - ronakgiriraj ======= 
+ ======== Linkedin - ronakgiriraj =======
+*/
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const handleNextClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "after";
+
+  nextGroup.dataset.status = "becoming-active-from-before";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
+
+const handlePreviousClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "before";
+
+  nextGroup.dataset.status = "becoming-active-from-after";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
