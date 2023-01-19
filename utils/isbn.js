2d1b107ffbb71aaad394e793eedce824f8f09228
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2023-01-18 21:24:38 -0800

    Added seed data

---
+++ utils/isbn.js
@@ -0,0 +1,31 @@
+// global variables
+let isbn = [];
+let myNum = 0;
+let checkDigit = 0;
+let total = 0;
+isbn.push(9, 7);
+
+// load the array
+thirdNum = Math.floor(Math.random() + 8);
+isbn.push(thirdNum);
+for (let i = 0; i < 9; i++) {
+  myNum = Math.floor(Math.random() * 10);
+  isbn.push(myNum);
+  if (myNum % 2 == 0) {
+    // odd numbers
+    total += myNum;
+  } else {
+    // even numbers
+    total += myNum * 3;
+  }
+}
+
+// create the checksum digit
+checkDigit = 10 - (total % 10);
+if (checkDigit == 10) {
+  checkDigit = 0;
+}
+isbn.push(checkDigit);
+isbn = isbn.join('');
+console.log(isbn);
+// console.log(isbn, total, total % 10);
