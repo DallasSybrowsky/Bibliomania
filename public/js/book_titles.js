18c690cbe26b057802d17d6132e13e1ff9830c23
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2023-01-20 11:41:15 -0800

    Fixed ISBN transfer error. The ISBN being added to the database wasn't matching what the user liked. Resolved.

--- public/js/book_titles.js
+++ public/js/book_titles.js
@@ -1,16 +1,63 @@
 
 
+// const titleFetch = async (isbn) => {
+//   let coverURL = `https://openlibrary.org/search.json?q=${isbn}`;
+//   const response = await fetch(coverURL);
+//   const data = await response.json();
+
+//   if (!data.docs[0].title) {
+//     console.log("Not found");
+
+//   } else { 
+//     // console.log(data.docs[0].title); 
+//     return data.docs[0].title;
+//   }
+// };
+
+// // titleFetch("9780618968633");
+
+// const parentEl = document.querySelector(".like-div").children;
+// // console.log(parentEl);
+
+// const array = [];
+
+// const getisbn = async () => {
+//   for (let i = 0; i < parentEl.length; i++) {
+//     const element = parentEl[i];
+//     const id = element.id;
+//     array.push(id);
+//   }
+  
+//   for (let x = 0; x < array.length; x++) {
+//     const isbn = array[x];
+//     const title = await titleFetch(isbn);
+//     document.getElementById(isbn).textContent = title;
+//     console.log(title);
+//   }
+// };
+
+// getisbn();
+
+
+// // for (const key in parentEl) {
+// //   console.log(key[0].id);
+// // }
+
+
+
+
 const titleFetch = async (isbn) => {
-  let coverURL = `https://openlibrary.org/search.json?q=${isbn}`;
+  let coverURL = `https://openlibrary.org/isbn/${isbn}.json`;
   const response = await fetch(coverURL);
   const data = await response.json();
-
-  if (!data.docs[0].title) {
+  console.log(isbn);
+  console.log(data);
+  if (!data.title) {
     console.log("Not found");
 
   } else { 
     // console.log(data.docs[0].title); 
-    return data.docs[0].title;
+    return data.title;
   }
 };
 
@@ -42,6 +89,3 @@ getisbn();
 // for (const key in parentEl) {
 //   console.log(key[0].id);
 // }
-
-
-
